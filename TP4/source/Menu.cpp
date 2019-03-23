5b87c4a3107ee8e39256a8584d708bea2c6cf896
Adam Naoui <adam.naoui@polymtl.ca>
2019-03-23 18:23:06 -0400

    Ajout Menu

--- TP4/source/Menu.cpp
+++ TP4/source/Menu.cpp
@@ -23,7 +23,14 @@ Menu::Menu(string fichier, TypeMenu type) :
 
 Menu::~Menu()
 {
-	//TODO
+	for (int i = 0; i < listePlats_.size(); i++)
+	{
+		delete listePlats_[i];
+	}
+		
+	listePlatsVege_.clear();
+	listePlats_.clear();
+
 }
 
 Plat* Menu::allouerPlat(Plat* plat) {
@@ -33,13 +40,50 @@ Plat* Menu::allouerPlat(Plat* plat) {
 
 Menu::Menu(const Menu & menu) : type_(menu.type_)
 {
-	//TODO
+	if (this != &menu) {
+		for (int i = 0; i < menu.listePlats_.size(); i++) //Voir diagramme de classe
+			if (dynamic_cast<PlatBioVege*>(menu.listePlats_[i])) { // On push dans les deux vecteurs si le plat est Bio et Vege
+				listePlats_.push_back(dynamic_cast<PlatBioVege*>(allouerPlat(menu.listePlats_[i])));
+				listePlatsVege_.push_back(dynamic_cast<PlatBioVege*>(listePlats_[i]));
+			}
+			else if (dynamic_cast<PlatBio*>(menu.listePlats_[i])) // On push dans plats seulement si plat est Bio
+				listePlats_.push_back(dynamic_cast<PlatBio*>(allouerPlat(menu.listePlats_[i])));
+			else if (dynamic_cast<PlatVege*>(menu.listePlats_[i])) {  // On push dans les deux vecteurs
+				listePlats_.push_back(dynamic_cast<PlatVege*>(allouerPlat(menu.listePlats_[i])));
+				listePlatsVege_.push_back(dynamic_cast<PlatVege*>(listePlats_[i]));
+			}
+			else
+				listePlats_.push_back(allouerPlat(menu.listePlats_[i])); // Il s'agit  d'un plat ordinaire
+	}
   
 }
 
 Menu & Menu::operator=(const Menu & menu)
 {
-        //TODO
+	if (this != &menu) {
+		type_ = menu.type_;
+
+		for (int i = 0; i < listePlats_.size(); i++)
+			delete listePlats_[i];
+
+		listePlats_.clear();
+		listePlatsVege_.clear();
+
+		for (int i = 0; i < menu.listePlats_.size(); i++)			//Voir diagramme de classe
+			if (dynamic_cast<PlatBioVege*>(menu.listePlats_[i])) {	// On push dans les deux vecteurs si le plat est Bio et Vege
+				listePlats_.push_back(dynamic_cast<PlatBioVege*>(allouerPlat(menu.listePlats_[i])));
+				listePlatsVege_.push_back(dynamic_cast<PlatBioVege*>(listePlats_[i]));
+			}
+			else if (dynamic_cast<PlatBio*>(menu.listePlats_[i]))	// On push dans plats seulement si plat est Bio
+				listePlats_.push_back(dynamic_cast<PlatBio*>(allouerPlat(menu.listePlats_[i])));
+			else if (dynamic_cast<PlatVege*>(menu.listePlats_[i])) {
+				listePlats_.push_back(dynamic_cast<PlatVege*>(allouerPlat(menu.listePlats_[i])));
+				listePlatsVege_.push_back(dynamic_cast<PlatVege*>(listePlats_[i]));
+			}
+			else
+				listePlats_.push_back(allouerPlat(menu.listePlats_[i])); // Il s'agit d'un plat ordinaire
+	}
+	return *this;
 }
 
 // Getters.
@@ -52,7 +96,25 @@ vector<Plat*> Menu::getListePlats() const
 // Autres methodes.
 
 Menu& Menu::operator+=(owner<Plat*> plat) {
-        //TODO
+	if (dynamic_cast<Plat*>(plat) != nullptr) // s'assurer que le pointeur passée en parametre s'agit bien d'un plat
+	{
+
+		if (dynamic_cast<PlatBioVege*>(plat)) { // il s'agit d un plat vegetarien on push donc dans les deux vecteurs
+			listePlats_.push_back(dynamic_cast<PlatBioVege*>(plat));
+			listePlatsVege_.push_back(dynamic_cast<PlatBioVege*>(plat));
+		}
+		else if (dynamic_cast<PlatBio*>(plat)) {
+			listePlats_.push_back(dynamic_cast<PlatBio*>(plat));
+		}
+		else if (dynamic_cast<PlatVege*>(plat)) {  // il s'agit d un plat vegetarien on push donc dans les deux vecteurs
+			listePlats_.push_back(dynamic_cast<PlatVege*>(plat));
+			listePlatsVege_.push_back(dynamic_cast<PlatVege*>(plat));
+		}
+		else
+			listePlats_.push_back(plat); // Il s'agit  d'un plat ordinaire
+
+	}
+	return *this;
 }
 
 void Menu::lireMenu(const string& nomFichier) {
@@ -111,5 +173,12 @@ Plat* Menu::lirePlatDe(LectureFichierEnSections& fichier)
 
 ostream& operator<<(ostream& os, const Menu& menu)
 {   
-        //TODO
+	for (unsigned i = 0; i < menu.listePlats_.size(); ++i) {
+		menu.listePlats_[i]->afficherPlat(os);
+	}
+	os << endl << "MENU ENTIEREMENT VEGETARIEN " << endl;
+	for (unsigned i = 0; i < menu.listePlatsVege_.size(); ++i) {
+		menu.listePlatsVege_[i]->afficherVege(os);
+	}
+	return os;
 }
